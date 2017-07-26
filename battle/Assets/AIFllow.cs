2686ea64907364403274f7a4f62256f8948e9264
yboyu <yangboyu@imudges.com>
2017-07-26 15:45:24 +0800

    c
    c

---
+++ battle/Assets/AIFllow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFllow : MonoBehaviour {
+
+	public GameObject Target;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Target = GameObject.Find("man");
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		keepFllow ();
+	}
+	public void keepFllow()
+	{
+		transform.LookAt (Target.transform.position);
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+}
+class Target
+{
+	public Vector3 position;
+	public GameObject target;
+}
